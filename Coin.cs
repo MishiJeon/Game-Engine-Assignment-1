1780cab0a6c11b5f6bb97dc4ff1c0be1d86ef047
MishiJeon <74584987+MishiJeon@users.noreply.github.com>
2022-09-26 21:28:26 -0400

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            ScoreManager.instance.ChangeScore(888);
+            Destroy(gameObject);
+            
+            if (collision.collider.tag == "Enemy")
+            {
+                ScoreManager.instance.ChangeScore(999);
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
